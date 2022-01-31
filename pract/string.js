8398dd8584f3c92f3649f54c5bd0ba70e0b2a8f3
Rostyk <65834087+Rostyslavkoo@users.noreply.github.com>
2022-01-31 18:15:20 +0200

    fix

--- pract/string.js
+++ pract/string.js
@@ -1,14 +1,18 @@
+const STRING_LIMIT = 10;
+const TARGET_CHAR = 'м';
+
 function showVerticalMessage(str) {
 	let result = '';
-	if (str[0] === 'м') {
+	if (str[0] === TARGET_CHAR) {
 		str = str[0].toUpperCase() + str.substr(1);
 	}
-	if (str.length >= 10) {
-		str = str.substr(0, 10);
+	if (str.length >= STRING_LIMIT) {
+		str = str.substr(0, STRING_LIMIT);
 	}
 	for (let char of str) {
 		result += `${char} \n`;
 	}
+	
 	console.log(result);
 }
 showVerticalMessage('марафон');
