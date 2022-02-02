93f75db195971c87f7bc88c6f13a5cfaed6b3007
Rostyk <65834087+Rostyslavkoo@users.noreply.github.com>
2022-02-02 13:38:43 +0200

    arrays

---
+++ array/app.js
@@ -0,0 +1,30 @@
+// let styles = ['djaz', 'bluez'];
+// styles.push('rock-n-roll');
+// styles[Math.floor(styles.length / 2)] = 'kali';
+// console.log(styles.shift());
+// styles.unshift('rep','reggie')
+// console.log(styles);
+
+// const sumInput = (num1) => {
+//     let result = []
+//         while(true){
+//             let num1 = +prompt('num1', '');
+//             if(num1 === '' || num1 === null || !isFinite(num1)) break;
+//             result.push(num1)
+//         }
+
+//         let sum = 0
+//         for(let number of result){
+//             sum += number
+//         }
+//         return sum
+
+        
+
+// };
+
+
+
+// console.log(sumInput())
+
+arr = [1, -2, 3, 4, -9, 6]
