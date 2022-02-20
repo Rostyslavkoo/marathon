2f612375d2124f9b5f725c4ab7f93692eb71c515
Rostyk <65834087+Rostyslavkoo@users.noreply.github.com>
2022-02-20 18:13:56 +0200

    fix

--- pract/calculator/assets/js/main.js
+++ pract/calculator/assets/js/main.js
@@ -5,7 +5,6 @@ function calcNumber(event, number1, number2) {
 		multi: number1 * number2,
 		sub: number1 - number2,
 	};
-
 	if (event === 'div' && number2 == 0) {
 		return 'division by 0';
 	}
@@ -14,12 +13,10 @@ function calcNumber(event, number1, number2) {
 	}
 	return 'unknown operation';
 }
-
 const HEADER_SIZE = {};
-for (let i = 0; i <= 15; i++) {
-	HEADER_SIZE[i] = `${96 - 4.5 * i}px`;
+for (let i = 0; i <= 13; i++) {
+	HEADER_SIZE[i] = `${96 - 5 * i}px`;
 }
-
 const OPERATIONS = {
 	sum: {
 		role: '+',
@@ -95,7 +92,6 @@ function onClickOperation(operation) {
 	}
 	splitNull();
 }
-
 function onCalculate() {
 	let result = '';
 
@@ -106,6 +102,7 @@ function onCalculate() {
 	// when only one item in actions
 	if (actions.length === 1) {
 		calcHeader.innerHTML = actions[0] in OPERATIONS ? 0 : actions;
+		checkHeaderSize()
 		return;
 	}
 	let getOperations = actions.filter(e => typeof e === 'string'); // find count of operations in actions
@@ -186,7 +183,6 @@ function onClicClear() {
 function onUnion() {
 	// to be continued
 }
-
 function splitNull() {
 	let arr = calcHeader.innerHTML;
 	if (arr.startsWith(0)) {
@@ -195,6 +191,7 @@ function splitNull() {
 	}
 }
 function checkHeaderSize() {
+	console.log(calcHeader.innerHTML.length)
 	let actionsLength = calcHeader.innerHTML.length;
 		if (HEADER_SIZE[actionsLength]) {
 			calcHeader.style.fontSize = HEADER_SIZE[actionsLength];
