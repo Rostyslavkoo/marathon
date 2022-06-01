bae2de640aa2f7e597ce38d09a9a6a4c99759cc0
Rostyk <65834087+Rostyslavkoo@users.noreply.github.com>
2022-06-01 11:24:56 +0300

    rewrite class to function with useState

--- react/genderize/genderize-react/src/main.jsx
+++ react/genderize/genderize-react/src/main.jsx
@@ -1,101 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/css/style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-class Button extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		return (
-			<input
-				type='submit'
-				value={this.props.label}
-				className={this.props.className}
-			/>
-		);
-	}
+function Button(props) {
+	return (
+		<input type='submit' value={props.label} className={props.className} />
+	);
 }
-class TexInput extends React.Component {
-	constructor(props) {
-		super(props);
-		this.handleCange = this.handleCange.bind(this)
-
+function TexInput(props) {
+	function handleCange(e) {
+		props.onTextChange(e.target.value);
 	}
-	handleCange(e){
-		this.props.onTextChange(e.target.value)
-
-	}
-	render() {
-		return (
-			<div>
-				<input
-					type='text'
-					placeholder={this.props.label}
-					onChange={this.handleCange}
-				/>
-				<div style={{ height: '30px' }}>
-					{this.props.value.length > 0 && this.props.value.length < 3 && (
-						<div style={{ fontSize: '14px', color: '#B33D30' }}>
-							name is too short
-						</div>
-					)}
-					<p style={{ height: '30px', fontSize: '14px', color: 'grey' }}>
-						{this.props.gender ? `gender – ${this.props.gender}` : ''}
-					</p>
-				</div>
+	return (
+		<div>
+			<input type='text' placeholder={props.label} onChange={handleCange} />
+			<div style={{ height: '30px' }}>
+				{props.value.length > 0 && props.value.length < 3 && (
+					<div style={{ fontSize: '14px', color: '#B33D30' }}>
+						name is too short
+					</div>
+				)}
+				<p style={{ height: '30px', fontSize: '14px', color: 'grey' }}>
+					{props.gender ? `gender – ${props.gender}` : ''}
+				</p>
 			</div>
-		);
-	}
+		</div>
+	);
 }
 
-class Main extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = { value: '', gender: '' };
-		this.handleSubmit = this.handleSubmit.bind(this);
-		this.onChangeInput = this.onChangeInput.bind(this);
+function Main() {
+	const [value, setValue] = useState('');
+	const [gender, setGender] = useState('');
+
+	function onChangeInput(e) {
+		setValue(e);
 	}
-	onChangeInput(e) {
-		this.setState({ value: e, gender: '' });
-	}
-	handleSubmit(e) {
+	function handleSubmit(e) {
 		e.preventDefault();
-		this.onSent()
+		onSent();
 	}
-	async onSent() {
+
+	async function onSent() {
 		try {
 			const serverUrl = 'https://api.genderize.io';
-			const res = await (
-				await fetch(`${serverUrl}?name=${this.state.value}`)
-			).json();
-			this.setState({ gender: res.gender });
+			const res = await (await fetch(`${serverUrl}?name=${value}`)).json();
+			setGender(res.gender);
 		} catch (e) {
 			alert(e);
 		}
 	}
-	render() {
-		return (
-			<div className='container'>
-				<div>
-					<h2>Enter a name</h2>
-					<form onSubmit={this.handleSubmit}>
-						<div className='d-flex jusitify-start align-start mt-1'>
-							<TexInput
-								value={this.state.value}
-								gender={this.state.gender}
-								label='Type a name'
-								onTextChange={this.onChangeInput}
-							/>
-							<Button label='Sent' className='ml-1 btn' />
-						</div>
-					</form>
-				</div>
+	return (
+		<div className='container'>
+			<div>
+				<h2>Enter a name</h2>
+				<form onSubmit={handleSubmit}>
+					<div className='d-flex jusitify-start align-start mt-1'>
+						<TexInput
+							value={value}
+							gender={gender}
+							label='Type a name'
+							onTextChange={onChangeInput}
+						/>
+						<Button label='Sent' className='ml-1 btn' />
+					</div>
+				</form>
 			</div>
-		);
-	}
+		</div>
+	);
 }
 
 root.render(<Main />);
