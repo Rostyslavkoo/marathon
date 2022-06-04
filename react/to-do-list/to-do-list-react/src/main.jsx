fdd5d184e2788e860e5e681dc00cbfa1c7b08ca4
Rostyk <65834087+Rostyslavkoo@users.noreply.github.com>
2022-06-04 18:24:32 +0300

    react add new task

--- react/to-do-list/to-do-list-react/src/main.jsx
+++ react/to-do-list/to-do-list-react/src/main.jsx
@@ -1,9 +1,11 @@
 import ReactDOM from 'react-dom/client';
 import './assets/scss/common.scss';
+import { useEffect, useState } from 'react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-function TasksContainer(props) {
+function List(props) {
+	if (!props.tasks.length) return;
 	const taskList = props.tasks
 		.filter(item => item.status === props.type)
 		.map((item, key) => (
@@ -14,30 +16,90 @@ function TasksContainer(props) {
 			</div>
 		));
 
+	return <div className='todo-box'>{taskList}</div>;
+}
+
+function Task(props) {
+	function onChange(e) {
+		props.onChangeTaskName(e.target.value, props.type);
+	}
+	function onClickAdd() {
+		props.onInputTask(props.type);
+	}
+	return (
+		<div className='search__wrapper'>
+			<input
+				value={props.value}
+				type='text'
+				placeholder={props.placeholder}
+				onChange={e => onChange(e)}
+			/>
+			<div className='input-btn' onClick={onClickAdd}></div>
+		</div>
+	);
+}
+
+function Header(props) {
 	return (
 		<div className='high__container container'>
 			<h1>{props.type === 'low' ? 'Low' : 'Hight'}</h1>
-			<div className='search__wrapper'>
-				<input type='text' placeholder={props.placeholder} />
-				<div className='input-btn'></div>
-			</div>
-			<div className='todo-box'>{taskList}</div>
+			<Task
+				value={props.value}
+				placeholder={props.placeholder}
+				onInputTask={props.onAddTask}
+				onChangeTaskName={props.onChangeTaskName}
+				type={props.type}
+			/>
+			<List tasks={props.tasks} type={props.type} />
 		</div>
 	);
 }
 function App() {
-	const tasks = [
+	const [ImportantTaskValue, setImportantTaskValue] = useState('');
+	const [LowTaskValue, setLowTaskValue] = useState('');
+	const [tasks, setTasks] = useState([
 		{ value: 'hight task', status: 'hight', done: false },
-		{ value: 'low task', status: 'low', done: true },
-	];
+		{ value: 'low task', status: 'low', done: false },
+	]);
+
+	function handleAddTask(type) {
+		const taskValue = type === 'low' ? LowTaskValue : ImportantTaskValue;
+		if (!taskValue) return;
+		setTasks([
+			{
+				value: taskValue,
+				status: type,
+				done: false,
+			},
+			...tasks,
+		]);
+		changeTaskValue('', type);
+	}
+	function onChangeTaskName(e, type) {
+		changeTaskValue(e, type);
+	}
+	function changeTaskValue(value, type) {
+		type === 'low' ? setLowTaskValue(value) : setImportantTaskValue(value);
+	}
+
 	return (
 		<div className='wrapper'>
-			<TasksContainer
+			<Header
 				type='hight'
+				value={ImportantTaskValue}
 				placeholder='add important cases'
+				onAddTask={handleAddTask}
+				onChangeTaskName={onChangeTaskName}
 				tasks={tasks}
 			/>
-			<TasksContainer type='low' placeholder='add cases' tasks={tasks} />
+			<Header
+				type='low'
+				placeholder='add cases'
+				onAddTask={handleAddTask}
+				onChangeTaskName={onChangeTaskName}
+				tasks={tasks}
+				value={LowTaskValue}
+			/>
 		</div>
 	);
 }
