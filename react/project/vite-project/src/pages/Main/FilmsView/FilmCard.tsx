671c845b86de9eb71553d88c543140d4606c4632
Rostyk <65834087+Rostyslavkoo@users.noreply.github.com>
2022-06-22 13:17:24 +0300

    react add mocks

--- react/project/vite-project/src/pages/Main/FilmsView/FilmCard.tsx
+++ react/project/vite-project/src/pages/Main/FilmsView/FilmCard.tsx
@@ -3,28 +3,30 @@ import Typography from '@mui/material/Typography';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import Grid from '@mui/material/Grid';
-import Divider from '@mui/material/Divider';
-import CardActions from '@mui/material/CardActions';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import StarBorderIcon from '@mui/icons-material/StarBorder';
 import BookmarkBorderOutlinedIcon from '@mui/icons-material/BookmarkBorderOutlined';
 
-function FilmCard({ film }: { film: any }) {
+function FilmCard({
+	film: { id, poster_path, title, vote_average },
+}: {
+	film: any;
+}) {
 	return (
-		<Card sx={{ display: 'flex', width: 'auto', height: '300px' }}>
+		<Card sx={{ display: 'flex', width: '100%', height: '300px' }}>
 			<CardMedia
 				component='img'
-				sx={{ minWidth: 'auto' }}
-				image={film.path_img}
-				alt='Live from space album cover'
+				image={poster_path}
+				alt={title}
 				height='300'
+				width='100'
 			/>
 
-			<CardContent sx={{ pb: 0 }}>
+			<CardContent sx={{ pb: 0, minWidth: '60%' }}>
 				<Grid
 					container
-					alignItems='flex-end'
+					alignItems='start'
 					justifyContent='space-between'
 					direction='column'
 					sx={{ height: '100%' }}
@@ -35,7 +37,7 @@ function FilmCard({ film }: { film: any }) {
 							color='text.secondary'
 							gutterBottom
 						>
-							Rating: {film.rating}
+							Rating: {vote_average}
 						</Typography>
 						<Grid item>
 							<IconButton color='secondary' size='small'>
@@ -48,7 +50,7 @@ function FilmCard({ film }: { film: any }) {
 					</Grid>
 					<Grid item>
 						<Typography variant='body2' color='text.secondary'>
-							{film.title}
+							{title}
 						</Typography>
 					</Grid>
 					<Grid item sx={{ width: '100%' }}>
--- react/project/vite-project/src/pages/Main/Filters/FilterSorted/SortedCheckboxList.tsx
+++ react/project/vite-project/src/pages/Main/Filters/FilterSorted/SortedCheckboxList.tsx
@@ -7,24 +7,80 @@ import Typography from '@mui/material/Typography';
 
 const checkboxValue = [
 	{
-		value: 'science-fiction',
-		label: 'Science Fiction',
+		id: 28,
+		name: 'боевик',
 	},
 	{
-		value: 'war',
-		label: 'War',
+		id: 12,
+		name: 'приключения',
 	},
 	{
-		value: 'westerns',
-		label: 'Westerns',
+		id: 16,
+		name: 'мультфильм',
 	},
 	{
-		value: 'drama',
-		label: 'Drama',
+		id: 35,
+		name: 'комедия',
 	},
 	{
-		value: 'crime-gangster',
-		label: 'Crime & Gangster',
+		id: 80,
+		name: 'криминал',
+	},
+	{
+		id: 99,
+		name: 'документальный',
+	},
+	{
+		id: 18,
+		name: 'драма',
+	},
+	{
+		id: 10751,
+		name: 'семейный',
+	},
+	{
+		id: 14,
+		name: 'фэнтези',
+	},
+	{
+		id: 36,
+		name: 'история',
+	},
+	{
+		id: 27,
+		name: 'ужасы',
+	},
+	{
+		id: 10402,
+		name: 'музыка',
+	},
+	{
+		id: 9648,
+		name: 'детектив',
+	},
+	{
+		id: 10749,
+		name: 'мелодрама',
+	},
+	{
+		id: 878,
+		name: 'фантастика',
+	},
+	{
+		id: 10770,
+		name: 'телевизионный фильм',
+	},
+	{
+		id: 53,
+		name: 'триллер',
+	},
+	{
+		id: 10752,
+		name: 'военный',
+	},
+	{
+		id: 37,
+		name: 'вестерн',
 	},
 ];
 function SortedCheckboxList() {
@@ -38,12 +94,13 @@ function SortedCheckboxList() {
 
 	return (
 		<>
-			<FormGroup sx={{ p: 1, minWidth: '100%' }}>
+			<FormGroup sx={{ px: 1, minWidth: '100%' }}>
 				{checkboxValue.map(checkbox => (
 					<FormControlLabel
-						key={checkbox.value}
-						control={<Checkbox size='small' />}
-						label={checkbox.label}
+						
+						key={checkbox.id}
+						control={<Checkbox size='small' sx={{ py: '1px' }} />}
+						label={checkbox.name}
 					/>
 				))}
 			</FormGroup>
